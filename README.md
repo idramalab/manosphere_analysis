ddd0f60ec5992bc45222591a7bbbbe45fb4b9f99
manoelhortaribeiro <manoelhortaribeiro@gmail.com>
2019-09-12 19:51:16 +0200

    Fixes bug + creates script for perspective helper.

--- README.md
+++ README.md
@@ -1,18 +1,11 @@
 ## Commands to Make Helper Files
 
-     python ./scripts/get_helper.py \
-     --src /data/savvas/incels/data/incels_comments_reddit_lower.ndjson \
-     --src /data/savvas/incels/data/inceltears_submissions.ndjson \
-     --src /data/savvas/incels/data/json/control/reddit/gaming/reddit_gaming_comments.ndjson \
-     --src /data/savvas/incels/data/json/control/reddit/random/reddit_random_comments.ndjson \
-     --dst /data/savvas/incels/data/
+     python ./scripts/get_helper.py --src /data/savvas/incels/data/incels_comments_reddit_lower.ndjson --src /data/savvas/incels/data/inceltears_comments.ndjson --src /data/savvas/incels/data/json/control/reddit/gaming/reddit_gaming_comments.ndjson --src /data/savvas/incels/data/json/control/reddit/random/reddit_random_comments.ndjson --kind all --kind all --kind gaming --kind random --dst /data/savvas/incels/data/
      
 and
  
-    python ./scripts/get_helper.py \
-    --src /data/savvas/incels/data/incels_comments_reddit_lower.ndjson \
-    --src /data/savvas/incels/data/inceltears_submissions.ndjson \
-    --src /data/savvas/incels/data/json/control/reddit/gaming/reddit_gaming_comments.ndjson \
-    --src /data/savvas/incels/data/json/control/reddit/random/reddit_random_comments.ndjson \
-    --dst /data/savvas/incels/data/ \
-    --author_dict
+    python ./scripts/get_helper.py --src /data/savvas/incels/data/incels_comments_reddit_lower.ndjson --src /data/savvas/incels/data/inceltears_comments.ndjson --src /data/savvas/incels/data/json/control/reddit/gaming/reddit_gaming_comments.ndjson --src /data/savvas/incels/data/json/control/reddit/random/reddit_random_comments.ndjson --kind all --kind all --kind gaming --kind random --dst /data/savvas/incels/data/ --author_dict
+
+and
+
+     python ./scripts/get_perspective.py --src /data/savvas/incels/data/json/perspective/perspective_redditall_results.txt --dst /data/savvas/incels/data/perspective_dict.sqlite
--- scripts/get_helper.py
+++ scripts/get_helper.py
@@ -13,6 +13,9 @@ parser.add_argument("--src", dest="src", action="append",
 parser.add_argument("--author_dict", dest="author_dict", action="store_true",
                     help="If present, runs author_dict, otherwise, runs for channel_dict.")
 
+parser.add_argument("--kind", dest="kinds", action="append",
+                    help="(all|random|gaming)")
+
 parser.add_argument("--dst", dest="dst", default="/data/incels_helpers/",
                     help="If present, runs author_dict, otherwise, runs for channel_dict.")
 
@@ -24,11 +27,13 @@ dict_db = SqliteDict(dst, tablename=table_name, journal_mode="OFF")
 tmp_dict = dict()
 
 fs = [open(f, "r") for f in args.src]
+kinds = [k for k in args.kinds]
 
 count = 0
 now = datetime.datetime.now()
 
-for f in fs:
+for f, k in zip(fs, kinds):
+    print("---->", k)
     for line in f:
 
         count += 1
@@ -38,23 +43,24 @@ for f in fs:
             author = line["author"]
             dict_val = {
                         "timestamp": int(line["created_utc"]),
-                        "subreddit": line["subreddit"].lower()
+                        "subreddit": line["subreddit"].lower() if k == "all" else k,
+                        "id": line["id"]
                         }
             val = tmp_dict.get(author, [])
             val.append(dict_val)
             tmp_dict[author] = val
 
         else:  # -- updates channel_dict
-            subreddit = line["subreddit"].lower()
+            subreddit = line["subreddit"].lower() if k == "all" else k
             dict_val = {
                         "author": line["author"],
-                        "timestamp": int(line["created_utc"])
+                        "timestamp": int(line["created_utc"]),
                         }
             val = tmp_dict.get(subreddit, [])
             val.append(dict_val)
             tmp_dict[subreddit] = val
 
-        if count % 1000000 == 0:
+        if count % 2500000 == 0:
 
             for key, item in tmp_dict.items():
                 try:
@@ -78,7 +84,7 @@ for f in fs:
         except AttributeError:
             print("AttributeError", key)
             continue
+        print(datetime.datetime.now() - now)
+        now = datetime.datetime.now()
     dict_db.commit()
-    dict_db.close()
-
-tmp_dict = {}
+dict_db.close()
